283265c06c29ec19f595b64735c9ad0f11ef12d4
DIANA <156034720+NethDG@users.noreply.github.com>
2024-02-09 14:03:36 -0600

    02:03

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.clear("Hola mundo");
 //crear una aplicacion vue. pero la sintaxis esta equivocada
 //deberia ser como el siguiente
 var diversion = Vue.createApp({
-
+    template: "<h1>Hola tonotos</h1>",
 
 });
+    diversion.mount("#main");
