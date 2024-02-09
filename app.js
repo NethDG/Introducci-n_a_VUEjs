e22efcb93b7bd91d8ad37c11f881cbe78f5f9747
DIANA <156034720+NethDG@users.noreply.github.com>
2024-02-09 14:02:03 -0600

    02:01

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 //limpia la consola
 console.clear("Hola mundo");
+
+//crear una aplicacion vue. pero la sintaxis esta equivocada
+//deberia ser como el siguiente
+var diversion = Vue.createApp({
+
+
+});
